2d70a2b8ac3e6afc66620fb362e201fd2bb7e063
jusyl <sylvester.yuan@gmail.com>
2014-10-29 22:44:44 +0800

    my

---
+++ src/com/jusyl/test/boss/tp/Test1.java
@@ -0,0 +1,16 @@
+package com.jusyl.test.boss.tp;
+
+public class Test1 {
+	private int iTest = 1;
+	private static String strTest = "今天是个好日子";
+	
+	public int out(){
+		System.out.println("");
+		return 1;
+	}
+	
+	public static void main(){
+        System.out.println("我想说：" + strTest);
+	}
+
+}
---
+++ src/com/jusyl/test/boss/tp/Test1Test.java
@@ -0,0 +1,24 @@
+package com.jusyl.test.boss.tp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Test1Test {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
